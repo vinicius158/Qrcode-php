33c99f4beda64ffbd36b26fe1591b95e6b11a54b
vinicius158 <77473673+vinicius158@users.noreply.github.com>
2025-02-19 14:31:04 -0300

    Update processar.php

--- processar.php
+++ processar.php
@@ -1,1 +1,20 @@
+<?php    
+    
+header("Content-Type: application/json");      
 
+require("enviando.php");    
+
+if(isset($_POST["txt"])){
+
+$txt = $_POST["txt"];     
+
+$obj = new Gerando();            
+
+$response = $obj->qr_code($txt);        
+
+echo json_encode($response);           
+
+}
+
+
+?>    
